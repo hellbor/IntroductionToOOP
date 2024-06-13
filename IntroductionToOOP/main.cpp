c7a1d531b7ea164fc2c4e3cd7dd686c15b6e2afa
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-06-13 21:46:51 +0300

    Fraction in progress

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -4,6 +4,8 @@ using std::cin;
 using std::cout;
 using std::endl;
 
+#define delimeter "\n-----------------------------------------------------\n"
+
 class Point
 {
 	double x;
@@ -17,11 +19,11 @@ public:
 	{
 		return y;
 	}
-	double set_x(double x)
+	void set_x(double x)
 	{
 		this->x = x;
 	}
-	double set_y(double y)
+	void set_y(double y)
 	{
 		this->y = y;
 	}
@@ -43,18 +45,47 @@ public:
 		this->y = y;
 		cout << "Constructor:\t\t" << this << endl;
 	}
+	Point(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyConstructor:\t" << this << endl;
+	}
 	~Point()
 	{
 		cout << "Destructor:\t\t" << this << endl;
 	}
 
+	//					Operators:
+	Point& operator=(const Point& other)
+	{
+		this->x = other.x;
+		this->y = other.y;
+		cout << "CopyAssignment:\t\t" << this << endl;
+		return *this;
+	}
+	Point& operator++()
+	{
+		x++;
+		y++;
+		return *this;
+	}
+	Point operator++(int)
+	{
+		Point old = *this;
+		x++;
+		y++;
+		return old;
+	}
 
 	//					Methods:
-	double distance(Point other)
+	double distance(const Point& other)const
 	{
 		double x_distance = this->x - other.x;
 		double y_distance = this->y - other.y;
 		double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
+		//sqrt() - Square Root (Квадратный корень)
+		//this->x *= 10;
 		return distance;
 	}
 	void print()const
@@ -63,7 +94,7 @@ public:
 	}
 };
 
-double distance(Point A, Point B)
+double distance(const Point& A, const Point& B)
 {
 	double x_distance = A.get_x() - B.get_x();
 	double y_distance = A.get_y() - B.get_y();
@@ -71,9 +102,34 @@ double distance(Point A, Point B)
 	return distance;
 }
 
+Point operator+(const Point& left, const Point& right)
+{
+	Point result;
+	result.set_x(left.get_x() + right.get_x());
+	result.set_y(left.get_y() + right.get_y());
+	return result;
+}
+
+bool operator==(const Point& left, const Point& right)
+{
+	/*if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
+		return true;
+	else
+		return false;*/
+	return left.get_x() == right.get_x() && left.get_y() == right.get_y();
+}
+
+std::ostream& operator<<(std::ostream& os, const Point& obj)
+{
+	return os << "X = " << obj.get_x() << "\tY = " << obj.get_y();
+}
+
 //#define STRUCT_POINT
-#define CONSTRUCTORS_CHECK
+//#define CONSTRUCTORS_CHECK
 //#define DISTANCE_CHECK
+//#define ASSIGNMENT_CHECK
+//#define ARITHMETICAL_OPERATORS_CHECK
+//#define COMPARISON_OPERATORS_CHECK
 
 void main()
 {
@@ -101,8 +157,15 @@ void main()
 
 	Point C(2, 3);
 	C.print();
-#endif // CONSTRUCTORS_CHECK
 
+	Point D = C;	//Copy constructor
+	D.print();
+
+	Point E;
+	E = D;			//Copy Assignment
+	E.print();
+
+#endif // CONSTRUCTORS_CHECK
 
 #ifdef DISTANCE_CHECK
 	Point A(2, 3);
@@ -111,10 +174,59 @@ void main()
 	A.print();
 	B.print();
 
+	cout << delimeter << endl;
 	cout << "Расстояние от точки 'A' до точки 'B':" << A.distance(B) << endl;
+	cout << delimeter << endl;
 	cout << "Расстояние от точки 'B' до точки 'A':" << B.distance(A) << endl;
+	cout << delimeter << endl;
 	cout << "Расстояние между точками 'A' и 'B': " << distance(A, B) << endl;
+	cout << delimeter << endl;
 	cout << "Расстояние между точками 'B' и 'A': " << distance(B, A) << endl;
+	cout << delimeter << endl;
 #endif // DISTANCE_CHECK
 
+#ifdef ASSIGNMENT_CHECK
+	int a, b, c;
+	a = b = c = 0;
+	cout << a << "\t" << b << "\t" << c << endl;
+
+	Point A, B, C;
+
+	cout << delimeter << endl;
+	A = B = C = Point(2, 3);	//Point(2, 3) - явно вызываем конструктор
+	//и создаем временный безымянный объект,
+	//этот объект и передается как параметр в первый CopyAssignment.
+	cout << delimeter << endl;
+	A.print();
+	B.print();
+	C.print();
+	cout << delimeter << endl;
+#endif // ASSIGNMENT_CHECK
+
+#ifdef ARITHMETICAL_OPERATORS_CHECK
+	Point A(2, 3);
+	A.print();
+
+	Point B(7, 8);
+	B.print();
+	Point C = A + B;
+	C.print();
+
+	Point D = C++;
+	C.print();
+	D.print();
+#endif // ARITHMETICAL_OPERATORS_CHECK
+
+#ifdef COMPARISON_OPERATORS_CHECK
+
+	//cout << (2 == 3) << endl;
+	Point(2, 3);
+	Point(7, 8);
+	//cout << (A == B) << endl;
+	cout << (Point(2, 3) == Point(7, 8)) << endl;
+#endif // COMPARISON_OPERATORS_CHECK
+
+	Point A(2, 3);
+
+	cout << A << endl;
 }
