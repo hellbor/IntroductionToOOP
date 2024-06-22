0253be59320822ecfb9199fd1d1afc53c4bf838e
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-06-22 22:02:41 +0300

    String on master

---
+++ String/main.cpp
@@ -0,0 +1,121 @@
+﻿#include<iostream>
+using namespace std;
+
+#define delimiter "\n-----------------------------------\n"
+
+class String
+{
+	int size;
+	char* str;
+public:
+	int get_size()const
+	{
+		return size;
+	}
+	const char* get_str()const
+	{
+		return str;
+	}
+	char* get_str()
+	{
+		return str;
+	}
+	
+	//			Constructors:
+	explicit String(int size = 80)
+	{
+		this->size = size;
+		this->str = new char[size] {};
+		cout << "DefaultConstructor:\t" << this << endl;
+	}
+	String(const char str[])
+	{
+		this->size = strlen(str) + 1;
+		//Функция strlen() возвращает размер строки в символах,
+		//и нам нужно добавить еще один байт для NULL-Terminator-a
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = str[i];
+		cout << "Constructor:\t" << this << endl;
+	}
+	String(const String& other)
+	{
+		//Deep copy (Побитовое копирование):
+		this->size = other.size;
+		this->str = new char[size]{};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyConsructor:" << this << endl;
+	}
+	~String()
+	{
+		delete[] str;
+		cout << "Destructor:\t" << this << endl;
+	}
+
+	//			Operators:
+	String& operator=(const String& other)
+	{
+		//Deep copy (Побитовое копирование):
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)this->str[i] = other.str[i];
+		cout << "CopyAssignment:\t" << this << endl;
+		return *this;
+	}
+
+	//			Methods:
+	void print()const
+	{
+		cout << "Obj:\t\t" << this << endl;
+		cout << "Size:\t\t" << size << endl;
+		cout << "Addr:\t\t" << &str << endl;
+		cout << "Str:\t\t" << str << endl;
+		cout << delimiter << endl;
+	}
+};
+
+String operator+(const String& left, const String& right)
+{
+	cout << delimiter << endl;
+	cout << "Operator+" << endl;
+	String buffer(left.get_size() + right.get_size() - 1);
+	buffer.print();
+	for (int i = 0; i < left.get_size(); i++)
+		buffer[i] = left[i];
+		//buffer.get_str()[i] = left.get_str()[i];
+	for (int i = 0; i < right.get_size(); i++)
+		buffer[i + left.get_size() - 1] = right[i];
+		//buffer.get_str()[i + left.get_size() - 1] = right.get_str()[i];
+	return buffer;
+}
+
+std::ostream& operator<<(std::ostream& os, const String& obj)
+{
+	return os << obj.get_str();
+}
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	String str1;		//Default constructor
+	str1.print();
+
+	//String str2 = 8;	//Conversion from 'int' to 'String'
+	String str2(8);		//SingleArgument 'int' constructor
+	str2.print();
+
+	String str3 = "Hello";
+	str3 = str3;
+	str3.print();
+
+	String str4 = "World";
+	str4.print();
+
+	cout << str3 << endl;
+	cout << str4 << endl;
+
+	//String str5 = str3 + str4;
+	String str5;
+	str5 = str3 + str4;		//Copy assignment
+	str5.print();
+	cout << str5 << endl;
+}
